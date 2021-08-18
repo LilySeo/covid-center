291f9e0c6bbe2bd22cb570613cdc2b2459b53ac3
Lily Seo <purism02@gmail.com>
2021-08-18 22:08:59 +0900

    맵 뿌리기까지 성공 :)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-// import React, { useState } from 'react';
-// import axios from "axios";
-
 import React from 'react';
 import CenterList from "./components/CenterList";
-
-
-const { kakao } = window;
 
 const App = () => {
     return <CenterList />
 }
-
-//
-// const App = () => {
-//   const [data, setData] = useState(null);
-//   const onClick = () => {
-//     axios.get(`https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=10&serviceKey=data-portal-test-key`)
-//         .then(response => {
-//           setData(response.data);
-//         });
-//   };
-//   return (
-//       <div>
-//         <div>
-//           <button onClick={onClick}>불러오기</button>
-//         </div>
-//         {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} /> }
-//       </div>
-//   )
-//
-// }
 
 export default App;
--- src/components/CenterItem.js
+++ src/components/CenterItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from "styled-components";
 
 const CenterItemBlock = styled.div`
@@ -9,14 +9,31 @@ const CenterItemBlock = styled.div`
   }
 
   .contents {
+    width: 100%;
     margin-bottom: 3rem;
+    
+    
+    &> div { 
+     float: left;
+     width: 300px;
+     height: 200px;
+     margin-right: 40px;
+     border: 1px solid #c5c5c5;
+     
+     &:hover {
+        opacity: 0.6;
+     }
+    }
+    
     h2 {
       margin: 0;
-      
+      padding-top: 12px;
+      color: #333;
     }
 
     p {
       margin: 0;
+      color: #666;
       line-height: 1.5;
       margin-top: 0.5rem;
       white-space: normal;
@@ -25,13 +42,35 @@ const CenterItemBlock = styled.div`
   
 `;
 
+const {kakao} = window;
+
 const CenterItem = ( { data }) => {
-    const { address, centerName, centerType, facilityName, lat} = data;
+    const { address, centerName, centerType, facilityName, lat, lng } = data;
+
+    const options = {
+        center: new kakao.maps.LatLng(lat, lng),
+        level: 9
+    };
+
+    const container = useRef(null);
+
+    useEffect(() => {
+        const map = new kakao.maps.Map(container.current, options);
+        // 마커가 표시될 위치입니다
+        let markerPosition = new kakao.maps.LatLng(lat, lng);
+        // 마커를 생성합니다
+        let marker = new kakao.maps.Marker({
+            position: markerPosition
+        });
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
+        map.setDraggable(false);
+    }, []);
 
     return (
         <CenterItemBlock>
             <div className="contents">
-
+                <div ref={container}></div>
                 <h2>{address}</h2>
                 <p>{centerName}</p>
                 <p>{centerType}</p>
